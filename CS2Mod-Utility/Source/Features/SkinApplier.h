26b3a7be2eed6598fdee3ac725c4468e8a3f18fa
qveezzx <guydennnooo2@gmail.com>
2026-04-03 22:44:39 -0700

    small fixes just like my d

--- CS2Mod-Utility/Source/Features/SkinApplier.h
+++ CS2Mod-Utility/Source/Features/SkinApplier.h
@@ -5,9 +5,7 @@
 #include <Features/SkinDatabase.h>
 #include <Features/MusicKits.h>
 #include <Features/WeaponDetection.h>
-#include <GameClient/EntitySystem/EntitySystem.h>
 #include <HookContext/HookContextMacros.h>
-#include <optional>
 #include <cstdint>
 
 namespace skin_changer {
@@ -23,15 +21,8 @@ public:
 
     void applySkinsAndMusicKit() noexcept
     {
-        if (!GET_CONFIG_VAR(skins_vars::Enabled) && !GET_CONFIG_VAR(music_kits_vars::Enabled))
-            return;
-
         if (GET_CONFIG_VAR(skins_vars::Enabled)) {
             applySkins();
-        }
-
-        if (GET_CONFIG_VAR(music_kits_vars::Enabled)) {
-            applyMusicKit();
         }
     }
 
@@ -60,44 +51,11 @@ private:
         if (weaponPtr == 0)
             return;
 
-        // Get weapon definition index to determine what type of weapon this is
-        // For now, we apply vanilla (Paint ID 0) as a default
-        // In production, this would read the equipped skin from SkinManager
-        
         // Write paint kit (vanilla = 0)
-        const auto paintKit = 0u;
-        
         // m_nFallbackPaintKit offset = 0x1540
         auto paintKitPtr = reinterpret_cast<std::uint32_t*>(weaponPtr + 0x1540);
-        *paintKitPtr = paintKit;
-    }
-
-    void applyMusicKit() noexcept
-    {
-        auto playerController = hookContext.localPlayerController();
-        if (!playerController)
-            return;
-
-        // Read selected music kit from config
-        const auto selectedMusicKitId = GET_CONFIG_VAR(music_kits_vars::SelectedMusicKit);
-
-        // PlayerController is a wrapper - convert to raw pointer for offset operations
-        auto controllerRawPtr = playerController.operator*();
-        auto controllerAddr = reinterpret_cast<std::uintptr_t>(controllerRawPtr);
-
-        // Get inventory services from player controller
-        // Offset: m_pInventoryServices = 0x700
-        if (controllerAddr == 0)
-            return;
-
-        auto inventoryServicesPtr = *reinterpret_cast<std::uintptr_t*>(controllerAddr + 0x700);
-
-        if (inventoryServicesPtr == 0)
-            return;
-
-        // Write music kit ID to m_unMusicID (offset 0x40)
-        auto musicKitIdPtr = reinterpret_cast<std::uint16_t*>(inventoryServicesPtr + 0x40);
-        *musicKitIdPtr = selectedMusicKitId;
+        if (paintKitPtr)
+            *paintKitPtr = 0u;
     }
 };
 
